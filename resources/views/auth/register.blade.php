7532f3b5e4d6c7dd6435b89f943cdc8cbef3573c
Webicultor <34300888+Webicultor@users.noreply.github.com>
2022-09-21 06:22:55 +0200

    Add files via upload
    1st

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Regístrate en DevStagram
+@endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-6/12 p-5">
+            <p><img src="{{ asset('img/registrar.jpg') }}" alt="Registro de usuarios"></p>
+        </div>
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form action="{{ route('register') }}" method="POST">
+                @csrf
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nombre
+                    </label>
+                    <input id="name" name="name" type="text" placeholder="Tu Nombre" class="border p-2 w-full rounded-lg @error('name') border-red-500 @enderror" value="{{ old('name') }}">
+                    @error('name')
+                        <p class="bg-red-500 text-white my-2 p-2 rounded-lg">{{$message}}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Username
+                    </label>
+                    <input id="username" name="username" type="text" placeholder="Tu Nombre de usuario" class="border p-2 w-full rounded-lg @error('username') border-red-500 @enderror" value="{{ old('username') }}">
+                    @error('username')
+                        <p class="bg-red-500 text-white my-2 p-2 rounded-lg">{{$message}}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        email
+                    </label>
+                    <input id="email" name="email" type="email" placeholder="Tu Correo Electrónico" class="border p-2 w-full rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+                    @error('email')
+                        <p class="bg-red-500 text-white my-2 p-2 rounded-lg">{{$message}}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Password
+                    </label>
+                    <input id="password" name="password" type="password" placeholder="Tu Contraseña"  class="border p-2 w-full rounded-lg @error('username') border-red-500 @enderror">
+                    @error('password')
+                        <p class="bg-red-500 text-white my-2 p-2 rounded-lg">{{$message}}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Repetir Password
+                    </label>
+                    <input id="password_confirmation" name="password_confirmation" type="password" placeholder="Repite tu Contraseña" class="border p-2 w-full rounded-lg">
+                </div>
+
+                <input type="submit" value="Registrarse" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+            </form>
+        </div>
+    </div>
+@endsection
+
